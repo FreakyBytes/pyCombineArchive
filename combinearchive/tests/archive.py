80875ca5f71431b5d90b874c0b56d5adf70a05f6
Martin Peters <martin@freakybytes.net>
2016-01-28 11:47:43 +0200

    extended reading test class a bit, no real testing yet, though

--- combinearchive/tests/archive.py
+++ combinearchive/tests/archive.py
@@ -2,6 +2,9 @@
 Test Classes for reading, writing and manipulating COMBINE archives
 no metadata tests involved
 """
+import shutil
+import os
+import tempfile
 import unittest
 from test import test_support
 
@@ -14,11 +17,17 @@ class ReadTest(unittest.TestCase):
     """
 
     def setUp(self):
-        self.carchive = combinearchive.CombineArchive('test.omex')
+        # first copy test archive into a temp location, so validation data does
+        # not get harmed
+        self.archive_location = tempfile.NamedTemporaryFile(delete=False).name
+        shutil.copy('test.omex', self.archive_location)
+        self.carchive = combinearchive.CombineArchive(self.archive_location)
         pass
 
     def tearDown(self):
+        # close archive and remove temp file
         self.carchive.close()
+        os.remove(self.archive_location)
         pass
 
 
