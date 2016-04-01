afc883c7310191c47147eb59ff5b11e2e5a632d9
Martin Peters <martin@freakybytes.net>
2016-04-01 12:23:13 +0200

    added return statements for zinfo

--- combinearchive/custom_zip.py
+++ combinearchive/custom_zip.py
@@ -2,7 +2,50 @@
 Read and write ZIP files.
 
 modified version of the original Python 2.7 zipfile module
+Copyright (c) 2001-2016 Python Software Foundation; All Rights Reserved
 """
+# 1. This LICENSE AGREEMENT is between the Python Software Foundation ("PSF"), and
+#    the Individual or Organization ("Licensee") accessing and otherwise using Python
+#    2.7.11 software in source or binary form and its associated documentation.
+#
+# 2. Subject to the terms and conditions of this License Agreement, PSF hereby
+#    grants Licensee a nonexclusive, royalty-free, world-wide license to reproduce,
+#    analyze, test, perform and/or display publicly, prepare derivative works,
+#    distribute, and otherwise use Python 2.7.11 alone or in any derivative
+#    version, provided, however, that PSF's License Agreement and PSF's notice of
+#    copyright, i.e., "Copyright (c) 2001-2016 Python Software Foundation; All Rights
+#    Reserved" are retained in Python 2.7.11 alone or in any derivative version
+#    prepared by Licensee.
+#
+# 3. In the event Licensee prepares a derivative work that is based on or
+#    incorporates Python 2.7.11 or any part thereof, and wants to make the
+#    derivative work available to others as provided herein, then Licensee hereby
+#    agrees to include in any such work a brief summary of the changes made to Python
+#    2.7.11.
+#
+# 4. PSF is making Python 2.7.11 available to Licensee on an "AS IS" basis.
+#    PSF MAKES NO REPRESENTATIONS OR WARRANTIES, EXPRESS OR IMPLIED.  BY WAY OF
+#    EXAMPLE, BUT NOT LIMITATION, PSF MAKES NO AND DISCLAIMS ANY REPRESENTATION OR
+#    WARRANTY OF MERCHANTABILITY OR FITNESS FOR ANY PARTICULAR PURPOSE OR THAT THE
+#    USE OF PYTHON 2.7.11 WILL NOT INFRINGE ANY THIRD PARTY RIGHTS.
+#
+# 5. PSF SHALL NOT BE LIABLE TO LICENSEE OR ANY OTHER USERS OF PYTHON 2.7.11
+#    FOR ANY INCIDENTAL, SPECIAL, OR CONSEQUENTIAL DAMAGES OR LOSS AS A RESULT OF
+#    MODIFYING, DISTRIBUTING, OR OTHERWISE USING PYTHON 2.7.11, OR ANY DERIVATIVE
+#    THEREOF, EVEN IF ADVISED OF THE POSSIBILITY THEREOF.
+#
+# 6. This License Agreement will automatically terminate upon a material breach of
+#    its terms and conditions.
+#
+# 7. Nothing in this License Agreement shall be deemed to create any relationship
+#    of agency, partnership, or joint venture between PSF and Licensee.  This License
+#    Agreement does not grant permission to use PSF trademarks or trade name in a
+#    trademark sense to endorse or promote products or services of Licensee, or any
+#    third party.
+#
+# 8. By copying, installing or otherwise using Python 2.7.11, Licensee agrees
+#    to be bound by the terms and conditions of this License Agreement.
+
 import struct, os, time, sys, shutil
 import binascii, cStringIO, stat
 import io
@@ -1117,7 +1160,8 @@ class ZipFile(object):
 
     def write(self, filename, arcname=None, compress_type=None):
         """Put the bytes from filename into the archive under the name
-        arcname."""
+        arcname.
+        Returns the zinfo"""
         if not self.fp:
             raise RuntimeError(
                   "Attempt to write to ZIP archive that was already closed")
@@ -1205,10 +1249,13 @@ class ZipFile(object):
         self.filelist.append(zinfo)
         self.NameToInfo[zinfo.filename] = zinfo
 
+        return zinfo
+
     def writestr(self, zinfo_or_arcname, bytes, compress_type=None):
         """Write a file into the archive.  The contents is the string
         'bytes'.  'zinfo_or_arcname' is either a ZipInfo instance or
-        the name of the file in the archive."""
+        the name of the file in the archive.
+        Returns the zinfo"""
         if not isinstance(zinfo_or_arcname, ZipInfo):
             zinfo = ZipInfo(filename=zinfo_or_arcname,
                             date_time=time.localtime(time.time())[:6])
@@ -1255,6 +1302,8 @@ class ZipFile(object):
         self.fp.flush()
         self.filelist.append(zinfo)
         self.NameToInfo[zinfo.filename] = zinfo
+
+        return zinfo
 
     def __del__(self):
         """Call the "close()" method in case the user forgot."""
